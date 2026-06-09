c10dd644857143fe219825b90e0a1eaf1529ac5f
liljemery <jeremyinoa67@gmail.com>
2026-06-09 09:44:54 -0400

    Update DuplaLogo component to use new logo image
    - Changed the logo image source in the DuplaLogo component from 'duplalogo.jpeg' to 'logo-dupla.png'. This update ensures the correct branding is displayed in the application.

--- frontend/src/components/DuplaLogo.tsx
+++ frontend/src/components/DuplaLogo.tsx
@@ -2,8 +2,6 @@ type Props = {
   className?: string
 }
 
-export function DuplaLogo({
-  className = 'h-12 w-auto max-w-[280px] object-contain object-left',
-}: Props) {
-  return <img src="/duplalogo.jpeg" alt="Grupo Dupla" className={className} />
+export function DuplaLogo({ className = 'h-12 w-auto max-w-[280px] object-contain object-left' }: Props) {
+  return <img src="/logo-dupla.png" alt="Grupo Dupla" className={className} />
 }
